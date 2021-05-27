ee11bd30d9de9c4cb035a105305a578384b709c4
Patryk Plaza <atrykp0@gmail.com>
2021-05-27 06:55:42 +0200

    add custom error handling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import productRouter from "./Routes/productRoutes.js";
 
@@ -20,6 +21,10 @@ app.use("/api/products", productRouter);
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
