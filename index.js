debb31feff97a1ca82c6dc97439a8da02e6be8c2
Patryk Plaza <atrykp0@gmail.com>
2021-05-27 05:56:14 +0200

    add product routes

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import colors from "colors";
-
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
+import productRouter from "./Routes/productRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -16,17 +15,10 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
+app.use("/api/products", productRouter);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
-});
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((prod) => prod._id === req.params.id);
-
-  res.json(product);
 });
 
 const PORT = process.env.PORT || 5000;
