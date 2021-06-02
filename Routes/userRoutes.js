69f46945ec0d0610b894aeb814dda1a9f16d2161
Patryk Plaza <atrykp0@gmail.com>
2021-06-02 06:05:34 +0200

    add user registration and password encrypt option

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { authUsers, getUserProfile } from "../controllers/userControllers.js";
+import {
+  authUsers,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/login", authUsers);
+userRouter.post("/", registerUser);
 userRouter.get("/profile", protect, getUserProfile);
 
 export default userRouter;
