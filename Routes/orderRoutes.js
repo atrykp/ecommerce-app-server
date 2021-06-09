8cb0119945e1bfec569a25889b936e473a40468e
Patryk Plaza <atrykp0@gmail.com>
2021-06-09 05:55:56 +0200

    add pay route

--- Routes/orderRoutes.js
+++ Routes/orderRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { addOrderItems, getOrderById } from "../controllers/orderController.js";
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", protect, addOrderItems);
 userRouter.get("/:id", protect, getOrderById);
+userRouter.put("/:id/pay", protect, updateOrderToPaid);
 
 export default userRouter;
